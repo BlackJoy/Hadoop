045afceabcdfdf67daef1009eb73e034800b69a4
BlackJoy <754095923@qq.com>
2017-01-04 09:54:04 +0800

    HadoopTest

---
+++ HadoopTest/src/Test.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+//
+//import org.apache.hadoop.io.IntWritable;
+//import org.apache.hadoop.io.Text;
+//import org.apache.hadoop.mapreduce.Mapper;
+//import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		StringTokenizer temp = new StringTokenizer(sc.next().toString());
+		while(temp.hasMoreTokens()){
+			
+		}
+		 
+	}
+
+	
+	
+
+}
